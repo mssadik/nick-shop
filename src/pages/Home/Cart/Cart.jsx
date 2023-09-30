90a9a8a326e28a7f0b28dfbb3439ba0dd7e094f5
Sadik <anarul6777@gmail.com>
2023-09-30 14:41:21 +0600

    check out cart design is done

--- src/pages/Home/Cart/Cart.jsx
+++ src/pages/Home/Cart/Cart.jsx
@@ -7,7 +7,8 @@ import CartItem from "./CartItem";
 const Cart = () => {
     const dispatch = useDispatch();
     const isTrue = useSelector((state) => state.cartSlice.cart);
-    const cartItems = useSelector((state) => state.cartSlice.cartItem)
+    const cartItems = useSelector((state) => state.cartSlice.cartItems)
+    // console.log(cartItems);
 
 
 
@@ -21,10 +22,21 @@ const Cart = () => {
                 {
                     cartItems?.length === 0 ? <CartEmpty></CartEmpty> :
                         <div>
-                            <div>
+                            <div className="flex py-3 px-5 items-start justify-start flex-col gap-y-7 lg:gap-y-5 overflow-y-scroll h-[81vh] scroll-smooth scroll-hidden">
                                 {
                                     cartItems?.map((item, i) =><CartItem key={i} item={item}></CartItem>)
                                 }
+                            </div>
+
+                            <div className="fixed bottom-0 bg-white w-full px-5 py-3 space-y-2 grid items-center">
+                                <div className="flex items-center justify-between">
+                                    <h1 className="text-base font-semibold uppercase">Sub Total</h1>
+                                    <h1 className="text-sm rounded bg-theme-cart text-slate-100 px-1 py-0.5">00</h1>
+                                </div>
+                                <div className="gird w-full items-center gap-2 space-y-3">
+                                    <p className="text-sm font-medium to-current">Texes and shipping will calculate at shipping</p>
+                                    <button className="button-theme w-full bg-theme-cart text-white">Check Out</button>
+                                </div>
                             </div>
                         </div>
                 }
--- src/pages/Home/Cart/CartItem.jsx
+++ src/pages/Home/Cart/CartItem.jsx
@@ -1,10 +1,34 @@
-
-const CartItem = ({item}) => {
-    const {id, title, text, img, color, shadow, price, quantity} = item;
+import {MinusIcon, PlusIcon, TrashIcon} from '@heroicons/react/24/outline'
+const CartItem = ({ item }) => {
+    const { id, title, text, img, color, shadow, price, quantity } = item;
+    console.log(item);
     return (
-        <div>
-            <h2>Item</h2>
-        </div>
+        <>
+            <div className='flex items-center justify-between w-full px5'>
+                <div className='flex items-center gap-5'>
+                    <div className={`bg-gradient-to-b ${color} ${shadow} relative rounded p-3 hover:scale-105 ease-in-out grid items-center`}><img className='w-36 h-auto object-fill lg:w-28' src={img} alt="" /></div>
+                    <div className='grid items-center gap-4'>
+                        <div className='grid items-center leading-none'>
+                            <h1 className='font-medium text-lg text-slate-900 lg:text-sm'>{title}</h1>
+                            <p className='text-sm text-slate-900 lg:text-xs'>{text}</p>
+                        </div>
+                        <div className='flex items-center justify-around w-full'>
+                            <button className="bg-theme-cart rounded w-6 h-6 lg:w-5 lg:h-5 flex items-center justify-center active:scale-90"><MinusIcon className='w-5 h-5 lg:w-4 lg:-h4 text-white storke-[2]'></MinusIcon></button>
+                            <div className='bg-theme-cart rounded text-white font-medium lg:text-xs w-7 h-6 lg:h-6 flex items-center justify-center'>{quantity}</div>
+                            <button className="bg-theme-cart rounded w-6 h-6 lg:w-5 lg:h-5 flex items-center justify-center active:scale-90"><PlusIcon className='w-5 h-5 lg:w-4 lg:-h4 text-white storke-[2]'></PlusIcon></button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className='grid items-center justify-center'>
+                        <h1 className='text-lg lg:text-base text-slate-900 font-medium'>{price * quantity}</h1>
+                    </div>
+                    <div className='grid items-center justify-center'>
+                        <button className="bg-theme-cart rounded w-6 h-6 lg:w-5 lg:h-5 flex items-center justify-center active:scale-90"><TrashIcon className='w-5 h-5 lg:w-4 lg:-h4 text-white storke-[2]'></TrashIcon></button>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
